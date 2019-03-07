2721ed09572c425c8921be0d0cc16b1a92c6ec9b
Abe D <iaryiou111@gmail.com>
2019-03-07 19:08:00 +0000

    Automatically backed up by Learn

--- random_grocery_generator.rb
+++ random_grocery_generator.rb
@@ -25,7 +25,7 @@ end
 
 def generate_cart
 	[].tap do |cart|
-		rand(10..20).times do
+		rand(20).times do
 			cart.push(items.sample)
 		end
 	end
@@ -58,27 +58,7 @@ end
 #binding.pry
 
 def consolidate_cart(cart)
-  merged_cart = []
-  cart.each do |item|
-    item_name = item.keys[0]
-    if merged_cart != []
-      merged_cart_items = []
-      merged_cart.each {|i| merged_cart_items << i.keys[0]}
-      #binding.pry
-      if merged_cart_items.include?(item_name) == false
-        merged_cart << item
-        merged_cart[-1][item_name][:count] = 1
-      else
-        mc_index = merged_cart.index{|i| i.keys[0] == item_name}
-        merged_cart[mc_index][item_name][:count] += 1
-      end
-    else
-      merged_cart << item
-      merged_cart[-1][item_name][:count] = 1
-    end
-  end
-  #binding.pry
-  merged_cart
+
 end
 
 
