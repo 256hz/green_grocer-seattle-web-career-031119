8347e76946a10f130acb3a0ba0296ef731b8afe7
Abe D <iaryiou111@gmail.com>
2019-03-07 19:56:28 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -5,27 +5,27 @@ def consolidate_cart(cart)
   merged_cart = {}
   cart.each do |item|
     item_name = item.keys[0]
-    puts "processing #{item_name}"
-    puts item
-    puts "cart.size = #{cart.size}"
-    puts "merged_cart.size = #{merged_cart.size}"
+#    puts "processing #{item_name}"
+#    puts item
+#    puts "cart.size = #{cart.size}"
+#    puts "merged_cart.size = #{merged_cart.size}"
     if merged_cart.size > 1                               #if items in cart, check for duplicate
-      puts "merged_cart not empty"
-      puts "#{merged_cart.keys}"
+#      puts "merged_cart not empty"
+#      puts "#{merged_cart.keys}"
       if merged_cart.keys.include?(item_name) == false    #new item's name not found in merged items
         merged_cart[item_name] = {price: item[item_name][:price], clearance: item[item_name][:clearance], count: 1}
-        puts "#{item} not found in merged_cart, added"
+#        puts "#{item} not found in merged_cart, added"
       else                                                #new item's name found
         merged_cart[item_name][:count] += 1
-        puts "duplicate item found, count = #{merged_cart[item_name][:count]}"
+#        puts "duplicate item found, count = #{merged_cart[item_name][:count]}"
       end
     else #add item if empty cart
-      puts "cart was empty"
+#      puts "cart was empty"
       merged_cart[item_name] = {price: item[item_name][:price], clearance: item[item_name][:clearance], count: 1}
-      puts "merged_cart:\n#{merged_cart}\n"
+#      puts "merged_cart:\n#{merged_cart}\n"
     end
-    puts merged_cart + "\n"
-    puts "it should loop now\n"
+#    puts merged_cart + "\n"
+#    puts "it should loop now\n"
   end
   merged_cart
 end
