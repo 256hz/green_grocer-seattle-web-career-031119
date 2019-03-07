421539e7dd9245d0170753b2ad3975f8342301eb
Abe D <iaryiou111@gmail.com>
2019-03-07 19:19:56 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -7,15 +7,19 @@ def consolidate_cart(cart)
     if merged_cart != []
       merged_cart_items = []
       merged_cart.each {|i| merged_cart_items << i.keys[0]}
+      puts "#{merged_cart_items}"
       #binding.pry
       if merged_cart_items.include?(item_name) == false
         merged_cart << item
+        puts "#{item} not found in merged_cart, added"
         merged_cart[-1][item_name][:count] = 1
       else
         mc_index = merged_cart.index{|i| i.keys[0] == item_name}
         merged_cart[mc_index][item_name][:count] += 1
+        puts "duplicate item found, count = #{merged_cart[mc_index][item_name][:count]}"
       end
     else #add item if empty cart
+      puts "cart was empty"
       merged_cart << item
       merged_cart[-1][item_name][:count] = 1
     end
